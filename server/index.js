c7898f1959b537c33af8b6482cb5b562ca4adc4f
BobbyShittu <shittu@etsu.edu>
2018-02-14 10:05:57 -0500

    git commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const Keys = require('./config/Keys')
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' })
-});
+passport.use(new GoogleStrategy({
+    clientID: Keys.googleClientId,
+    clientSecret: Keys.googleClientSecret,
+    callbackURL:'/auth/google/callback'
+}, acessToken => {
+   console.log(acessToken)
+    }));
+
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope:['profile','email']
+}))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
